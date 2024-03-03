69cc32532997a845bba7d6a53cc594549a93e421
LiosK <contact@mail.liosk.net>
2024-03-03 15:26:14 +0900

    refactor: move EtagHasher to lib crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::{fmt, mem, num};
 
 pub trait Md5Hasher: Default {
     type Output: AsRef<[u8]> + Into<[u8; 16]>;
@@ -26,5 +26,74 @@ impl Md5Hasher for md5::Md5 {
 
     fn finalize_reset(&mut self) -> Self::Output {
         md5::Digest::finalize_reset(self)
+    }
+}
+
+#[derive(Debug)]
+pub struct EtagHasherMulti<H> {
+    chunksize: num::NonZeroUsize,
+    n_chunks: usize,
+    hasher_whole: H,
+    hasher_chunk: H,
+    current_capacity: usize,
+}
+
+impl<H: Md5Hasher> EtagHasherMulti<H> {
+    pub fn new(chunksize: num::NonZeroUsize) -> Self {
+        Self {
+            chunksize,
+            n_chunks: 0,
+            hasher_whole: Default::default(),
+            hasher_chunk: Default::default(),
+            current_capacity: chunksize.into(),
+        }
+    }
+
+    pub fn update(&mut self, data: impl AsRef<[u8]>) {
+        let mut buf = data.as_ref();
+        assert!(self.current_capacity > 0);
+        while buf.len() >= self.current_capacity {
+            let used = self.current_capacity;
+            self.hasher_chunk.update(&buf[..used]);
+            self.n_chunks += 1;
+            self.hasher_whole.update(self.hasher_chunk.finalize_reset());
+            self.current_capacity = self.chunksize.into();
+            buf = &buf[used..];
+        }
+        if !buf.is_empty() {
+            self.hasher_chunk.update(buf);
+            self.current_capacity -= buf.len();
+        }
+    }
+
+    pub fn finalize(mut self) -> Etag<impl AsRef<[u8]>> {
+        assert!(self.current_capacity <= self.chunksize.into());
+        let has_partial_chunk = self.current_capacity < self.chunksize.into();
+        if self.n_chunks == 0 || (self.n_chunks == 1 && !has_partial_chunk) {
+            Etag(self.hasher_chunk.finalize(), 1)
+        } else {
+            if has_partial_chunk {
+                self.n_chunks += 1;
+                self.hasher_whole.update(self.hasher_chunk.finalize());
+            }
+            Etag(self.hasher_whole.finalize(), self.n_chunks)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Etag<D>(D, usize);
+
+impl<D: AsRef<[u8]>> fmt::Display for Etag<D> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use fmt::Write as _;
+        let mut buf = arrayvec::ArrayString::<64>::new();
+        for e in self.0.as_ref() {
+            write!(buf, "{:02x}", e)?;
+        }
+        if self.1 > 1 {
+            write!(buf, "-{}", self.1)?;
+        }
+        fmt::Display::fmt(buf.as_str(), f)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::num::NonZeroUsize;
-use std::{error, fmt, fs, io, path, process};
+use std::{error, fs, io, path, process};
 
 #[cfg(unix)]
 use std::os::unix::ffi::OsStrExt as _;
 
-use s3etag::Md5Hasher;
+use s3etag::{EtagHasherMulti, Md5Hasher};
 
 fn main() -> process::ExitCode {
     const PROG: &str = env!("CARGO_PKG_NAME");
+    const CHUNKSIZE_HELP: &str =
+        "multipart_chunksize used for upload in bytes or with a size suffix KB, MB, GB, or TB";
     let matches = clap::Command::new(PROG)
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -43,7 +45,7 @@ fn main() -> process::ExitCode {
     };
     let chunksize = *matches.get_one::<NonZeroUsize>("chunksize").unwrap();
     for filename in matches.get_many::<path::PathBuf>("files").unwrap() {
-        let hasher = EtagHasher::<md5_impl::Md5>::new(chunksize);
+        let hasher = EtagHasherMulti::<md5_impl::Md5>::new(chunksize);
         if let Err(e) = process_file(filename, hasher, &mut writer, buffer.as_deref_mut()) {
             eprintln!("error: {}: {}", filename.display(), e);
             exit_code = process::ExitCode::FAILURE;
@@ -51,9 +53,6 @@ fn main() -> process::ExitCode {
     }
     exit_code
 }
-
-const CHUNKSIZE_HELP: &str =
-    "multipart_chunksize used for upload in bytes or with a size suffix KB, MB, GB, or TB";
 
 /// Parses the chunksize argument.
 fn parse_chunksize(s: &str) -> Result<NonZeroUsize, Box<dyn error::Error + Sync + Send>> {
@@ -78,7 +77,7 @@ fn parse_chunksize(s: &str) -> Result<NonZeroUsize, Box<dyn error::Error + Sync 
 /// Computes and prints the Etag for a file.
 fn process_file(
     filename: &path::Path,
-    mut hasher: EtagHasher<impl Md5Hasher>,
+    mut hasher: EtagHasherMulti<impl Md5Hasher>,
     writer: &mut impl io::Write,
     fread_buffer: Option<&mut [u8]>,
 ) -> io::Result<()> {
@@ -121,71 +120,6 @@ fn process_file(
     write!(writer, "{}", filename.display())?;
 
     writer.write_all(b"\n")
-}
-
-#[derive(Debug)]
-struct EtagHasher<H> {
-    chunksize: NonZeroUsize,
-    n_chunks: usize,
-    hasher_whole: H,
-    hasher_chunk: H,
-    current_capacity: usize,
-}
-
-impl<H: Md5Hasher> EtagHasher<H> {
-    fn new(chunksize: NonZeroUsize) -> Self {
-        Self {
-            chunksize,
-            n_chunks: 0,
-            hasher_whole: Default::default(),
-            hasher_chunk: Default::default(),
-            current_capacity: chunksize.into(),
-        }
-    }
-
-    fn update(&mut self, data: impl AsRef<[u8]>) {
-        let mut buf = data.as_ref();
-        assert!(self.current_capacity > 0);
-        while buf.len() >= self.current_capacity {
-            let used = self.current_capacity;
-            self.hasher_chunk.update(&buf[..used]);
-            self.n_chunks += 1;
-            self.hasher_whole.update(self.hasher_chunk.finalize_reset());
-            self.current_capacity = self.chunksize.into();
-            buf = &buf[used..];
-        }
-        self.hasher_chunk.update(buf);
-        self.current_capacity -= buf.len();
-    }
-
-    fn finalize(mut self) -> impl fmt::Display {
-        struct Etag<D>(D, usize);
-        impl<D: AsRef<[u8]>> fmt::Display for Etag<D> {
-            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                use fmt::Write as _;
-                let mut buf = arrayvec::ArrayString::<64>::new();
-                for e in self.0.as_ref() {
-                    write!(buf, "{:02x}", e)?;
-                }
-                if self.1 > 1 {
-                    write!(buf, "-{}", self.1)?;
-                }
-                fmt::Display::fmt(buf.as_str(), f)
-            }
-        }
-
-        assert!(self.current_capacity <= self.chunksize.into());
-        let has_partial_chunk = self.current_capacity < self.chunksize.into();
-        if self.n_chunks == 0 || (self.n_chunks == 1 && !has_partial_chunk) {
-            Etag(self.hasher_chunk.finalize(), 1)
-        } else {
-            if has_partial_chunk {
-                self.n_chunks += 1;
-                self.hasher_whole.update(self.hasher_chunk.finalize());
-            }
-            Etag(self.hasher_whole.finalize(), self.n_chunks)
-        }
-    }
 }
 
 #[cfg(feature = "openssl")]
